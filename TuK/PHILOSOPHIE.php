93c96b2aaee9ee72cb90f3c9bc9c69e5ce30baa7
Peculator <Sven_86@gmx.net>
2014-11-19 17:50:22 +0100

    Fixed navbar + read more

--- TuK/PHILOSOPHIE.php
+++ TuK/PHILOSOPHIE.php
@@ -6,18 +6,19 @@
 	<body>
 		<?php require("navbar.html"); ?>
 		<div class="container" id="content">
-			<div>
 				<div class="center90 row images">
-					<img src="images/bild_philosophie.png"></img>
+					<img style="float:left" src="images/bild_philosophie.png"></img>
 				</div>
-			</div>
 			<div class=" main">
 				<div class=" center90 container">
 					<div class="noPadding col-md-12">
 						<div class="contentLeft noPadding col-md-8">
+
+    					<div class="more lead">
+
 							<h1>HEILENDE KRÄFTE IM TANZ®</h1>
 							
-							<p class="lead">Heilende Kräfte im Tanz®, HKiT®, ist eine Einladung zum Leben.
+							Heilende Kräfte im Tanz®, HKiT®, ist eine Einladung zum Leben.
 							Dieser Ansatz hilft, Lebensenergie in schöpferische Kreativität, in Sinnlichkeit und
 							lebensbejahende Bahnen zu lenken, die uns erlauben, mit jedem Atemzug das Leben zu
 							feiern und unseren Körper mit jeder seiner Faser zu erspüren. Fühlen, wie das ganze
@@ -32,10 +33,11 @@
 							die Selbstheilungskräfte des Körpers aktiviert und wichtiges Handwerkszeug wie Vertrauen und
 							neue ungeahnte Beweglichkeit mit auf den Weg gegeben werden. </br></br>
 							Ich lade Sie ein in die große bewegende Welt der Heilenden Kräfte im Tanz® und in die heilsame Körpertherapie.
-							</br></br> <span class="blue">
+							<!-- </br></br> <span class="blue">
 							mehr lesen...
-							</span>
-							</p>
+							</span> -->
+							
+						</div>
 						</div>
 						<div class="noPadding contentRight col-md-4">
 							<h1 style="visibility:hidden">Zitate</h1>
--- TuK/js/script.js
+++ TuK/js/script.js
@@ -1,41 +1,138 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-// 	var content = $('#content');
-// 	var body = $('body');
-// 	var head = $('head');
+    // 	var content = $('#content');
+    // 	var body = $('body');
+    // 	var head = $('head');
 
-// // Names of the sites
- 	var startpage = "index.php";
- 	var page1 = "PHILOSOPHIE.php";
+    // // Names of the sites
+    var startpage = "index.php";
+    var page1 = "PHILOSOPHIE.php";
+    var size = 279;
 
- 	var currentPage = location.pathname.split('/').slice(-1)[0];
+    var currentPage = location.pathname.split('/').slice(-1)[0];
 
 
-	document.onscroll = scroll;
+    $(window).resize(function() {
+        origOffsetY = $('#content > .images > img').outerHeight();
+        console.log(origOffsetY);
+        resizeContent(origOffsetY);
 
-	function scroll() {
+    });
 
-		var size= 279;
+    function scroll() {
 
-	    if ($(window).scrollTop() >= size) {
-	        $('.navbar').addClass('navbar-fixed-top');
-	        $('.navbar').css('top','0px');
-	    } else {
-	        $('.navbar').removeClass('navbar-fixed-top');
-	        $('.navbar').css('top',size);
-	    }
-	}
+        var size = $('#content > .images > img').outerHeight();
+        var navSize = $('.navbar').outerHeight();
 
-	//Set Selected Chapter
-	if(currentPage==page1){
-		$('.page1').addClass('active');
-	}  
+        console.log(navSize);
 
-// 	$('ul.nav li.dropdown a').hover(function(){
-// 		console.log("hover");
-//        $(this).parent().children('ul.dropdown-menu').slideDown(); 
-//     }, function(){
-//        $(this).parent().children('ul.dropdown-menu').slideUp(); 
-// });
+        if ($(window).scrollTop() >= size) {
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.navbar').css('top', '0px');
+            $('.main').css('padding-top', navSize);
+        } else {
+            $('.navbar').removeClass('navbar-fixed-top');
+            $('.navbar').css('top', size);
+            $('.main').css('padding-top', navSize);
+        }
+    }
+
+    //Set Selected Chapter
+    if (currentPage == page1) {
+        $('.page1').addClass('active');
+    }
+
+    function resizeContent(offset) {
+        console.log("resize");
+        var size = $('#content > .images > img').outerHeight();
+        $('.item').each(function(index) {
+            $(this).css('height', offset);
+        });
+        $('.navbar').css('top', (offset));
+        scroll();
+    }
+
+    document.onscroll = scroll;
+    scroll();
+    resizeContent(size);
+
+    // 	$('ul.nav li.dropdown a').hover(function(){
+    // 		console.log("hover");
+    //        $(this).parent().children('ul.dropdown-menu').slideDown(); 
+    //     }, function(){
+    //        $(this).parent().children('ul.dropdown-menu').slideUp(); 
+    // });
+
+
+    // Mehr lesen
+
+    // Configure/customize these variables.
+    var showChar = 1000; // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Mehr lesen >";
+    var lesstext = "Weniger lesen";
+
+
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if (content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext + '</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function() {
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle(100);
+        $(this).prev().toggle(100);
+        return false;
+    });
+
+    $('.navbar-collapse').on('hidden.bs.collapse', function () {
+  		scroll();
+    });
+     $('.navbar-collapse').on('shown.bs.collapse', function () {
+  		//scroll();
+    });
+     $('.navbar-collapse').on('hide.bs.collapse', function () {
+  		//scroll();
+    });
+     $('.navbar-collapse').on('show.bs.collapse', function () {
+     	var navSize = $('.navbar').outerHeight();
+     	$('.main').css('padding-top', navSize+280);
+  		//scroll();
+    });
+
+     $('.dropdown').on('show.bs.dropdown', function () {
+		var navSize = $('.navbar').outerHeight();
+     	$('.main').css('padding-top', navSize);
+     	//scroll();
+	});
+
+          $('.dropdown').on('shown.bs.dropdown', function () {
+		var navSize = $('.navbar').outerHeight();
+     	$('.main').css('padding-top', navSize);
+     	//scroll();
+	});
+     $('.dropdown').on('hidden.bs.dropdown', function () {
+		var navSize = $('.navbar').outerHeight();
+     	$('.main').css('padding-top', navSize+280);
+     	scroll();
+	});
+
+        
 
 });
